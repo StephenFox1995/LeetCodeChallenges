39aab06e6f1ddc4e016f2c7a2efa60dc09b5b2d5
Stephen Fox <stephenfox995@hotmail.com>
2016-09-04 21:53:44 +0100

    283 solved.

--- 283-move_zeroes.c
+++ 283-move_zeroes.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 void moveZeroes(int* nums, int numSize) {
-  
+  for (int i = 0; i < numSize; i++) {
+    if (nums[i] == 0) { // If current number is 0, we want to push to end.
+      for (int j = i; j < numSize - 1; j++) {
+        nums[j] = nums[j + 1]; // Shift all numbers all the way back.
+      }
+      nums[numSize - 1] = 0;
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
-  
+  int nums[] = {0, 1, 0, 3, 12, 0, 11, 0};
+  int size = sizeof(nums) / sizeof(int);
+  moveZeroes(nums, size);
+  printf("[");
+  for (int i = 0; i < size; i++) {
+    printf("%d ", nums[i]);
+  }
+  printf("]\n");
 }
